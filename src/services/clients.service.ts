a646666ed6f902b031b060818248048edec5c68d
guilhermeguedes01 <guilherme.guedes@cazia.ai>
2026-06-16 18:55:03 -0300

    feature(client): add page client by id

--- src/services/clients.service.ts
+++ src/services/clients.service.ts
@@ -1,7 +1,12 @@
 import { api } from "@/libs/axios";
-import type { Client } from "@/types/client";
+import type { Client, ClientDetails } from "@/types/client";
 
 export async function getClientsService() {
   const response = await api.get<Client[]>("/clients");
+  return response.data;
+}
+
+export async function getClientByIdService(id: string) {
+  const response = await api.get<ClientDetails>(`/clients/${id}`);
   return response.data;
 }
--- src/types/client.ts
+++ src/types/client.ts
@@ -6,3 +6,23 @@ export interface Client {
   email?: string | null;
   createdAt: string;
 }
+
+export interface ClientEvent {
+  id: string;
+  title: string;
+  eventDate: string;
+  venueName: string;
+  city: string;
+  state: string;
+  artistName: string;
+}
+
+export interface ClientDetails {
+  id: string;
+  name: string;
+  companyName?: string | null;
+  phone?: string | null;
+  email?: string | null;
+  createdAt: string;
+  events: ClientEvent[];
+}
